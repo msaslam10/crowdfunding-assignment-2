2adf6dfa5ba173981066debdc0c39d62a13a0343
msaslam10 <shaheertohai@gmail.com>
2024-09-23 12:53:51 +0000

    displaying details

--- client/details.js
+++ client/details.js
@@ -1,4 +1,36 @@
-const urlParams = new URLSearchParams(window.location.search)
+const urlParams = new URLSearchParams(window.location.search);
 const fundraiserId = urlParams.get('id');
 
 console.log(fundraiserId);
+
+const fetchDetails = async (id) => {
+  console.log('Inside fetch details');
+  await fetch(`http://localhost:3000/api/fundraisers/${id}`)
+    .then((res) => res.json())
+    .then((fundraiser) => {
+      document.getElementById('fundraiser-caption').textContent =
+        fundraiser.CAPTION;
+      document.getElementById(
+        'fundraiser-organizer'
+      ).textContent = `Organizer: ${fundraiser.ORGANIZER}`;
+      document.getElementById(
+        'fundraiser-target'
+      ).textContent = `Target Funding: ${fundraiser.TARGET_FUNDING}`;
+      document.getElementById(
+        'fundraiser-current'
+      ).textContent = `Current Funding: ${fundraiser.CURRENT_FUNDING}`;
+      document.getElementById(
+        'fundraiser-city'
+      ).textContent = `City: ${fundraiser.CITY}`;
+      document.getElementById(
+        'fundraiser-category'
+      ).textContent = `Category: ${fundraiser.CATEGORY}`;
+      document.getElementById(
+        'fundraiser-description'
+      ).textContent = `Description: ${fundraiser.DESCRIPTION}`;
+    });
+};
+
+if (fundraiserId) {
+  fetchDetails(fundraiserId);
+}
